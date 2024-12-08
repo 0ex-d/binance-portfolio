f488b11a391dc9264db4d2270566fd9d221273c6
David <3173957+0ex-d@users.noreply.github.com>
2024-12-08 22:46:10 +0000

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"html/template"
 	"io"
-	"os"
-	"sort"
-	"strconv"
 	"strings"
-	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	log "github.com/sirupsen/logrus"
+	"goland.local/binance-portfolio/pkg"
 )
 
 type Template struct {
@@ -46,145 +40,9 @@ type IndexPage struct {
 
 var ErrorGenericResp = errors.New("error fetching data or pair doesn't exist for this user")
 
-func getApiAndSecretKeys() (string, string) {
-	apiKey := os.Getenv("BINANCE_API_KEY")
-	secretKey := os.Getenv("BINANCE_SECRET_KEY")
-	if apiKey == "" {
-		log.Fatal("You obviously didn't read the Readme.md! :( BINANCE_API_KEY and BINANCE_SECRET_KEY are required!")
-	}
-	return apiKey, secretKey
-}
-
-func getTs() string {
-	return strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10)
-}
-
-type Prices []float64
-
-type stas[T float64 | int] interface{}
-type RESTResp[T interface{} | map[string]interface{}] struct {
-	Data T
-	Err  interface{}
-}
-
-type PortfolioBalance struct {
-	Symbol             string  `json:"symbol"`
-	QuoteSymbol        string  `json:"quote_symbol"`
-	Free               float64 `json:"free"`
-	Locked             float64 `json:"locked"`
-	Price              float64 `json:"price"`
-	PriceFlag          string  `json:"price_flag"`
-	PriceChangeValue   float64 `json:"price_change_value"`
-	PriceChangePercent float64 `json:"price_change_percent"`
-	QuoteValue         float64 `json:"quote_value"`
-}
-
-func signParams(message, secret string) string {
-	mac := hmac.New(sha256.New, []byte(secret))
-	mac.Write([]byte(message))
-	return hex.EncodeToString(mac.Sum(nil))
-}
-
-var portfolioBalancesInMemory []PortfolioBalance
-
-func calculateRealizedPNL(trades []Trade, avgBuyPrice float64) (float64, error) {
-	var realizedPNL float64
-	for _, trade := range trades {
-		tradePrice, _ := strconv.ParseFloat(trade.Price, 64)
-		tradeQty, _ := strconv.ParseFloat(trade.Qty, 64)
-		if trade.IsBuyer {
-			continue
-		}
-		realizedPNL += (tradePrice - avgBuyPrice) * tradeQty
-	}
-	return realizedPNL, nil
-}
-
-func getPortfolioBalancesAndCCData(currency string) ([]PortfolioBalance, error) {
-	var err error
-	var balances []Balance
-	var portfolioBalances []PortfolioBalance
-	if len(portfolioBalancesInMemory) != 0 {
-		log.Info("[getPortfolioBalancesAndCCData]: Getting from memory")
-		return portfolioBalancesInMemory, err
-	}
-
-	balances, err = GetAccountBalances()
-	if err != nil {
-		return portfolioBalances, err
-	}
-
-	var ccDataInstruments []string
-	for _, balance := range balances {
-		if balance.Asset == "USDT" || balance.Asset == "GBP" || balance.Asset == "USD" || balance.Asset == currency {
-			continue
-		}
-		ccDataInstruments = append(ccDataInstruments, fmt.Sprintf("%s-%s", balance.Asset, currency))
-	}
-	spotResponse, err := GetCCDataCurrentTickerPrice(strings.Join(ccDataInstruments, ","), os.Getenv("CC_API_KEY"))
-	if err != nil {
-		return portfolioBalances, err
-	}
-
-	for _, balance := range balances {
-		if balance.Asset == "USDT" || balance.Asset == "GBP" || balance.Asset == "USD" || balance.Asset == currency {
-			portfolioBalances = append(portfolioBalances, PortfolioBalance{
-				Symbol: balance.Asset,
-				Free:   balance.Free,
-			})
-			continue
-		}
-		instrument := fmt.Sprintf("%s-%s", balance.Asset, currency)
-		currentInstrument := spotResponse.Data[instrument]
-		assetValue := balance.Free * currentInstrument.Price
-		portfolioBalances = append(portfolioBalances, PortfolioBalance{
-			Symbol:             balance.Asset,
-			QuoteSymbol:        currency,
-			Free:               balance.Free,
-			QuoteValue:         assetValue,
-			Price:              currentInstrument.Price,
-			PriceFlag:          currentInstrument.PriceFlag,
-			PriceChangeValue:   currentInstrument.CurrentDayChange,
-			PriceChangePercent: currentInstrument.CurrentDayChangePercentage,
-		})
-	}
-	sort.Slice(portfolioBalances, func(i, j int) bool {
-		return portfolioBalances[i].Free > portfolioBalances[j].Free
-	})
-	portfolioBalancesInMemory = portfolioBalances
-
-	return portfolioBalances, nil
-}
-
-func getTotalPortfolioValue(currency string) (float64, error) {
-	balances, err := GetAccountBalances()
-	if err != nil {
-		return 0, err
-	}
-
-	var totalValue float64
-	var instruments []string
-	for _, balance := range balances {
-		if balance.Asset == "USDT" || balance.Asset == "GBP" || balance.Asset == "USD" {
-			continue
-		}
-		instruments = append(instruments, fmt.Sprintf("%s-%s", balance.Asset, currency))
-	}
-	spotResponse, err := GetCCDataCurrentTickerPrice(strings.Join(instruments, ","), os.Getenv("CC_API_KEY"))
-	if err != nil {
-		return 0, err
-	}
-	for _, balance := range balances {
-		if balance.Asset == "USDT" || balance.Asset == "GBP" || balance.Asset == "USD" {
-			continue
-		}
-		instrument := fmt.Sprintf("%s-%s", balance.Asset, currency)
-		currentInstrument := spotResponse.Data[instrument]
-		assetValue := balance.Free * currentInstrument.Price
-		totalValue += assetValue
-	}
-	return totalValue, nil
-}
+var walletBalancesInMemory []*pkg.WalletBalance
+var portfolioBalancesInMemory []*pkg.PortfolioBalance
+var assetToTradesInMemory = make(map[string][]pkg.Trade)
 
 func main() {
 	var err error
@@ -212,8 +70,8 @@ func main() {
 		if strings.TrimSpace(limit) == "" {
 			limit = "1000"
 		}
-		var data []Order
-		data, err = GetAllOrders(symbol, limit)
+		var data []pkg.Order
+		data, err = pkg.GetAllOrders(symbol, limit)
 		return c.JSON(200, data)
 	})
 
@@ -226,52 +84,32 @@ func main() {
 		if strings.TrimSpace(limit) == "" {
 			limit = "1000"
 		}
-		var data []Trade
-		data, err = GetTradesList(symbol, limit)
+		var data []pkg.Trade
+		data, err = pkg.GetTradesList(symbol, limit)
 		return c.JSON(200, data)
 	})
 
-	e.GET("/wallet", func(c echo.Context) error {
-		var balances []PortfolioBalance
+	e.GET("/portfolio", func(c echo.Context) error {
+		var balances []*pkg.PortfolioBalance
 		currency := "USDT"
-		balances, err := getPortfolioBalancesAndCCData(currency)
+		balances, err := pkg.GetPortfolioBalancesAndCCData(currency, portfolioBalancesInMemory, assetToTradesInMemory)
 		if err != nil {
-			return c.JSON(400, RESTResp[[]PortfolioBalance]{Data: balances, Err: errors.New("error getting balances")})
+			return c.JSON(400, pkg.RESTResp[[]*pkg.PortfolioBalance]{Data: balances, Err: errors.New("error getting balances")})
 		}
-		return c.JSON(200, RESTResp[[]PortfolioBalance]{Data: balances})
+		return c.JSON(200, pkg.RESTResp[[]*pkg.PortfolioBalance]{Data: balances})
+	})
+	e.GET("/wallet", func(c echo.Context) error {
+		var balances []*pkg.WalletBalance
+		currency := "USDT"
+		balances, err := pkg.GetWalletBalancesAndCCData(currency, walletBalancesInMemory)
+		if err != nil {
+			return c.JSON(400, pkg.RESTResp[[]*pkg.WalletBalance]{Data: balances, Err: errors.New("error getting balances")})
+		}
+		return c.JSON(200, pkg.RESTResp[[]*pkg.WalletBalance]{Data: balances})
 	})
 
 	e.GET("/", func(c echo.Context) error {
-		headerTrs := []HeaderTr{{
-			Name:  "Asset",
-			Icon:  "fa fa-caret-up",
-			Class: "",
-		},
-			{
-				Name:  "Price",
-				Icon:  "fa fa-caret-up",
-				Class: "",
-			},
-			{
-				Name:  "Total Value(TV)",
-				Icon:  "fa fa-caret-up",
-				Class: "",
-			},
-			{
-				Name:  "PnL",
-				Icon:  "fa fa-caret-up",
-				Class: "",
-			},
-			{
-				Name:  "Change",
-				Icon:  "fa fa-caret-up",
-				Class: "",
-			},
-		}
-		tSection := TableSection{Header: headerTrs}
-		return c.Render(200, "index", IndexPage{
-			TableSection: tSection,
-		})
+		return c.Render(200, "index", IndexPage{})
 	})
 	port := "42000"
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
